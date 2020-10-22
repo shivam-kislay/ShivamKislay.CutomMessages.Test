e424f509ea5e17c4619a199f49ee7b47f102d77b
Shivam Kislay <shivam.kislay@iloy-group.com>
2020-10-22 15:39:23 +0100

    Add project files.

---
+++ ShivamKislay.CutomMessages.Test/CustomMessages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShivamKislay.CutomMessages.Test
+{
+    public static class CustomMessages
+    {
+        public static Dictionary<int, string> ErrorMessages = new Dictionary<int, string>()
+        {
+            {547, "New Custom Error Message"}
+        };
+    }
+}
